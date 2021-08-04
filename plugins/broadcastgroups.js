a1d5cf3d4f48294ddc1fc0f8d9264ee08afa0002
VjRFPS <62533236+VjRFPS@users.noreply.github.com>
2021-08-04 11:49:18 -0300

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, text }) => {
   let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
+  conn.reply(m.chat, `_Mensagem transmitida à ${groups.length} grupos_`, m)
   for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 Mensagem transmitida aos os grupos. 」\n' + randomID(32)), true).catch(_=>_)
   m.reply('Transmitido à todos os grupos!')
 }
