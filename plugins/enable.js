551662116bf53dd898c6848d043a152a92959c99
VjRFPS <62533236+VjRFPS@users.noreply.github.com>
2021-08-04 11:02:18 -0300

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -155,7 +155,7 @@ ${usedPrefix}disable welcome
       throw false
   }
   m.reply(`
-*${type}* foi *${isEnable ? 'com sucesso.' : 'mati'}kan* ${isAll ? 'untuk bot ini' : isUser ? '' : 'untuk chat ini'}
+*${type}* *${isEnable ? 'Ativado com sucesso' : 'Desativado com sucesso'}* ${isAll ? 'neste bot.' : isUser ? '' : 'neste chat.'}
 `.trim())
 }
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
