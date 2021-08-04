ee5c88d4786b7838302e0521e0facb4e5ed93b01
VjRFPS <62533236+VjRFPS@users.noreply.github.com>
2021-08-04 10:53:08 -0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,28 +7,15 @@ handler.before = function (m, { isAdmin, isBotAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    m.reply('Não toleramos link aqui.Apague o link.\n\n ${listAdmin}')
+    m.reply('Não toleramos link aqui.Apague o link.\n\n')
     if (global.opts['restrict']) {
       if (isAdmin || !isBotAdmin) return true
-      conn.groupRemove(m.chat, [user])
+      conn.groupRemove(m.chat, [m.sender])
     }
   }
   return true
-  
-  let handler = async (m, { conn, participants, groupMetadata }) => {
 
-    const getGroupAdmins = (participants) => {
-        admins = []
-        for (let i of participants) {
-            i.isAdmin ? admins.push(i.jid) : ''
-        }
-        return admins
-    }
-    
-    let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink } = global.db.data.chats[m.chat]
-        const groupAdmins = getGroupAdmins(participants)
-        let listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n')
-        let text = `*${listAdmin}*'
+
 }
 
 module.exports = handler
